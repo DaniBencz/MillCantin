e2b1dcf5a40b8339d4dfc3921bc878d1801b922e
Daniel <daniel.bencz@gmail.com>
2020-11-05 17:06:13 +0100

    start layout

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import github from './github.png'
+import logo from './logo1-100.png'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const getMenu = (letter: string) => {
+		console.log(letter)
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div id="repo">
+					<a href="https://github.com/DaniBencz/MillCantin" rel="noopener noreferrer" target="_blank">
+						<img alt="gitHub repo" src={github} width="60" height="60"></img>
+					</a>
+				</div>
+				<img alt="gitHub repo" src={logo} width="60" height="60"></img>
+			</header>
+			<div id="welcome">
+				<h2>MillCantin totally fake clone site</h2>
+				<h3>which menu are you interested in?</h3>
+			</div>
+			<div id="buttons">
+				<button onClick={() => getMenu('A')}>Menu A</button>
+				<button onClick={() => getMenu('B')}>Menu B</button>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
