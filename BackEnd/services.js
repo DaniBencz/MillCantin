ec831b5f41571eca762f86ddcf7b82436552222d
Daniel <daniel.bencz@gmail.com>
2020-11-05 10:48:20 +0100

    main allergenes

--- BackEnd/services.js
+++ BackEnd/services.js
@@ -22,22 +22,34 @@ const getMenuPage = (menuLetter) => {
 }
 
 const getMainDish = ($, menuLetter) => {
-	return new Promise((res, rej) => {
-		let main
-		// since page structure for the two menus are slightly different
-		// it has to be handled
-		if (menuLetter === 'A') {
-			let str = $('.woocommerce-product-details__short-description p:nth-child(4)')
-				.text().replace('Main course: ', '')
-			let indexOfLineBreak = str.indexOf("\n")
-			main = str.substring(indexOfLineBreak + 1)
-		}
-		else {
-			main = $('.woocommerce-product-details__short-description p:nth-child(5)')
-				.text().replace('Main course: ', '')
-		}
-		res({ $, main })
-	})
+	let main
+	// since page structure for the two menus are slightly different it has to be handled
+	if (menuLetter === 'A') {
+		let str = $('.woocommerce-product-details__short-description p:nth-child(4)')
+			.text().replace('Main course: ', '')
+		let indexOfLineBreak = str.indexOf("\n")
+		main = str.substring(indexOfLineBreak + 1)
+	}
+	else {
+		main = $('.woocommerce-product-details__short-description p:nth-child(5)')
+			.text().replace('Main course: ', '')
+	}
+	return main
+}
+
+const getMainAllergenes = ($, menuLetter) => {
+	let allergenes = []
+	if (menuLetter === 'A') {
+		$('.woocommerce-product-details__short-description p:nth-child(5) img').each((i, el) => {
+			allergenes.push(el.attribs.src)
+		})
+	}
+	else {
+		$('.woocommerce-product-details__short-description p:nth-child(6) img').each((i, el) => {
+			allergenes.push(el.attribs.src)
+		})
+	}
+	return allergenes
 }
 
 const scrapeMenuPage = (url, menuLetter) => {
@@ -50,10 +62,7 @@ const scrapeMenuPage = (url, menuLetter) => {
 		}
 
 		request(options)
-			.then($ => {
-				return getMainDish($, menuLetter)
-			})
-			.then(({ $, main }) => {
+			.then(($) => {
 				let soup_allergenes = []
 				$('.woocommerce-product-details__short-description p:nth-child(3) img').each((i, el) => {
 					soup_allergenes.push(el.attribs.src)
@@ -64,8 +73,8 @@ const scrapeMenuPage = (url, menuLetter) => {
 					soup: $('.woocommerce-product-details__short-description p:nth-child(2)')
 						.text().replace('Soup: ', ''),
 					soup_allergenes,
-					main,
-					// main_allergenes: $('.woocommerce-product-details__short-description p:nth-child(5)').text().replace('Soup: ', ''),
+					main: getMainDish($, menuLetter),
+					main_allergenes: getMainAllergenes($, menuLetter),
 				}
 				res(details)
 			})
